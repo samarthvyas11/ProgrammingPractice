c50c7364218e67d0b408041df4047736a5d46f90
Samarthvyas11 <70595427+samarthvyas11@users.noreply.github.com>
2020-09-17 22:21:59 +0530

    Update INTEST.py

--- INTEST/INTEST.py
+++ INTEST/INTEST.py
@@ -2,7 +2,7 @@ number_of_divisibility_test,dividend = map(int,input().split())
 count_of_divisible_number = 0
 for i in range(number_of_divisibility_test):
     divisor = int(input())
-    if divisor % dividend ==0:
+    if divisor % dividend == 0:
         count_of_divisible_number = count_of_divisible_number + 1
 print(count_of_divisible_number)        
         
