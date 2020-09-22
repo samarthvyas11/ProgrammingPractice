a77d8397b87fb9353ccded3842f2d3effb25ee4b
Samarthvyas11 <70595427+samarthvyas11@users.noreply.github.com>
2020-09-22 15:12:24 +0530

    Create FLOW001.py

---
+++ Codechef/FLOW001/testcases/FLOW001.py
@@ -0,0 +1,8 @@
+remaining_test_cases = int(input())
+while remaining_test_cases > 0:
+    first_number,second_number=map(int,input().split(" "))
+    add_of_numbers = first_number + second_number
+    print(add_of_numbers)
+    remaining_test_cases = remaining_test_cases - 1
+    
+    
